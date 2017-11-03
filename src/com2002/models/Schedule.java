78d3b8a2d3c0d7c38bbc147f6af0680b9f3f6fbb
Andreas Constantinou <aconstantinou3@sheffield.ac.uk>
2017-11-03 15:59:55 +0000

    Created Schedule class

---
+++ src/com2002/models/Schedule.java
@@ -0,0 +1,93 @@
+package com2002.models;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import com2002.models.*; 
+import com2002.utils.*;
+
+public class Schedule {
+	
+	public static ResultSet[] getAppointments() {
+		ResultSet[] schedule = new ResultSet[1000];
+		int count = 0;
+		try {
+			Connection conn = Database.getConnection();
+			ResultSet rs = execQuery("SELECT * FROM Appointments", conn);
+			while(rs.next()) {
+				schedule[count] = rs;
+				count++;
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return schedule;
+	}
+		
+	public static ResultSet[] getAppointmentsByDoctor(String userN, String pass) {
+		ResultSet[] schedule = new ResultSet[1000];
+		int count = 0;
+		try {
+			Connection conn = Database.getConnection();
+			ResultSet rs = execQuery("SELECT * FROM Appointments WHERE username = '" 
+					+ userN + "' AND password = '" + pass + "'", conn);
+			while(rs.next()) {
+				schedule[count] = rs;
+				count++;
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return schedule;
+	}
+	
+	public static ResultSet[] getAppointmentsByPatient(int patID) {
+		ResultSet[] schedule = new ResultSet[1000];
+		int count = 0;
+		try {
+			Connection conn = Database.getConnection();
+			ResultSet rs = execQuery("SELECT * FROM Appointments WHERE PatientID = " + patID + "", conn);
+			while(rs.next()) {
+				schedule[count] = rs;
+				count++;
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return schedule;
+	}
+	
+	
+	public void setAppointment(Timestamp start, Timestamp end, String userN, int patID, String nts, 
+			   AppointmentType treatmentN, int totalA, int currA) {
+		Appointment app = new Appointment(start, userN);
+		app.setAppointment(start, end, userN, patID, nts, treatmentN, totalA, currA);
+	}
+	
+	public void deleteAppointment(Timestamp start, String userN) {
+		Appointment app = new Appointment(start, userN);
+		app.removeAppointment( start, userN);
+	}
+	
+	private static ResultSet execQuery(String query, Connection conn) {
+		ResultSet rs = null;
+		try {
+			conn = Database.getConnection();
+			Statement stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	public static void main(String[] args) {
+	}
+	
+}
