6e7f532873f1ead995fd5bdeeeb6fbbb5ad574ed
Andreas Constantinou <aconstantinou3@sheffield.ac.uk>
2017-11-16 23:37:46 +0000

    Schedule class finished

--- src/com2002/models/Schedule.java
+++ src/com2002/models/Schedule.java
@@ -3,9 +3,7 @@ package com2002.models;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -14,35 +12,65 @@ import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationExceptio
 
 import com2002.utils.Database;
 
-public class Schedule {
+import com2002.models.Appointment;
 
-	public static ArrayList<Appointment> getAppointments() throws Exception {
+public class Schedule {
+	
+	/**
+	 * Returns all the appointments as an ArrayList of Appointments.
+	 * @return An ArrayList of all the Appointments.
+	 * @throws CommunicationsException when an error occurs whilst attempting connection.
+	 * @throws SQLException for any other error.
+	 */
+	public static ArrayList<Appointment> getAppointments() throws CommunicationsException, SQLException {
 		ArrayList<Appointment> appointments = new ArrayList<Appointment>();
 		Connection conn = Database.getConnection();
-		ResultSet rs = execQuery("SELECT * FROM Appointments", conn);
-		while (rs.next()) {
-			Timestamp startDate = rs.getTimestamp("StartDate");
-			String username = rs.getString("Username");
-			appointments.add(new Appointment(startDate, username));
+		try {
+			ResultSet rs = DBQueries.execQuery("SELECT * FROM Appointments", conn);
+			while (rs.next()) {
+				Timestamp startDate = rs.getTimestamp("StartDate");
+				String username = rs.getString("Username");
+				appointments.add(new Appointment(startDate, username));
+			}
+		} finally {
+			conn.close();
 		}
-		conn.close();
 		return appointments;
 	}
 
-	public static ArrayList<Appointment> getAppointmentsByDoctor(Doctor doctor) throws Exception {
+	/**
+	 * Returns all the appointments by doctor as an ArrayList of Appointments.
+	 * @param doctor A doctor, either hygienist or dentist.
+	 * @return An ArrayList of all the Appointments by doctor.
+	 * @throws CommunicationsException when an error occurs whilst attempting connection.
+	 * @throws SQLException for any other error, could be incorrect parameters.
+	 */
+	public static ArrayList<Appointment> getAppointmentsByDoctor(Doctor doctor) throws CommunicationsException, SQLException  {
 		ArrayList<Appointment> appointments = new ArrayList<Appointment>();
 		Connection conn = Database.getConnection();
 		String username = doctor.getUsername();
-		ResultSet rs = execQuery("SELECT * FROM Appointments WHERE Username = '" + username + "'", conn);
-		while (rs.next()) {
-			Timestamp startDate = rs.getTimestamp("StartDate");
-			appointments.add(new Appointment(startDate, username));
+		try {
+			ResultSet rs = DBQueries.execQuery("SELECT * FROM Appointments WHERE Username = '" + username + "'", conn);
+			while (rs.next()) {
+				Timestamp startDate = rs.getTimestamp("StartDate");
+				appointments.add(new Appointment(startDate, username));
+			}
+		} finally {
+			conn.close();
 		}
 		return appointments;
 	}
 	
+	/**
+	 * Returns all the appointments by doctor and day as an ArrayList of Appointments.
+	 * @param doctor A doctor, either hygienist or dentist.
+	 * @param date A date of the java class Date.
+	 * @return An ArrayList of all the Appointments by doctor and day.
+	 * @throws CommunicationsException when an error occurs whilst attempting connection.
+	 * @throws SQLException for any other error, could be incorrect parameters.
+	 */
 	@SuppressWarnings("deprecation")
-	public static ArrayList<Appointment> getAppointmentsByDoctorAndDay(Doctor doctor, Date date) throws Exception {
+	public static ArrayList<Appointment> getAppointmentsByDoctorAndDay(Doctor doctor, Date date) throws CommunicationsException, SQLException   {
 		ArrayList<Appointment> appointments = new ArrayList<Appointment>();
 		Connection conn = Database.getConnection();
 		String username = doctor.getUsername();
@@ -55,15 +83,27 @@ public class Schedule {
 		datePlusOne.setSeconds(0);
 		Timestamp s1 = new Timestamp(date.getTime());
 		Timestamp s2 = new Timestamp(datePlusOne.getTime());
-		ResultSet rs = execQuery("SELECT * FROM Appointments WHERE Username = '" + username + 
-				"' and StartDate >= '" + s1.toString() + "' and StartDate < '" + s2.toString() + "'", conn);
-		while (rs.next()) {
-			Timestamp startDate = rs.getTimestamp("StartDate");
-			appointments.add(new Appointment(startDate, username));
+		try {
+			ResultSet rs = DBQueries.execQuery("SELECT * FROM Appointments WHERE Username = '" + username + 
+					"' and StartDate >= '" + s1.toString() + "' and StartDate < '" + s2.toString() + "'", conn);
+			while (rs.next()) {
+				Timestamp startDate = rs.getTimestamp("StartDate");
+				appointments.add(new Appointment(startDate, username));
+			}
+		} finally {
+			conn.close();
 		}
 		return appointments;
 	}
-	public static ArrayList<Appointment> getAppointmentsByPatient(int patientID) throws Exception {
+	
+	/**
+	 * Returns all the appointments by patient as an ArrayList of Appointments.
+	 * @param patientID The Patient ID of a patient.
+	 * @return Returns all the appointments by patient as an ArrayList of Appointments.
+	 * @throws CommunicationsException when an error occurs whilst attempting connection.
+	 * @throws SQLException for any other error, could be incorrect parameters.
+	 */
+	public static ArrayList<Appointment> getAppointmentsByPatient(int patientID) throws CommunicationsException, SQLException  {
 		ArrayList<Appointment> appointments = new ArrayList<Appointment>();
 		Connection conn = Database.getConnection();
 		int patID = patientID;
@@ -80,44 +120,35 @@ public class Schedule {
 		}
 		return appointments;
 	}
-
-	public static ArrayList<Appointment> getDoctorAppointmentsByPatient(String username, Patient patient) throws Exception {
-		ArrayList<Appointment> appointments = new ArrayList<Appointment>();
-		Connection conn = Database.getConnection();
-		String patientId = String.valueOf(patient.getPatientID());
-		ResultSet rs = execQuery("SELECT * FROM Appointments WHERE PatientID = '" + patientId + "' and Username = '" + username + "'", conn);
-		while (rs.next()) {
-			Timestamp startDate = rs.getTimestamp("StartDate");
-			appointments.add(new Appointment(startDate, username));
-		}
-		return appointments;
-	}
 	
+	/**
+	 * Calls the appointment constructor to create an appointment.
+	 * @param start Timestamp of when the appointment should start.
+	 * @param end Timestamp of when the appointment should end.
+	 * @param userN Username of staff member conducting the appointment.
+	 * @param patID The patient's ID.
+	 * @param nts Any notes for the Appointment.
+	 * @param treatmentN The appointment type (Remedial, Cleaning, etc.).
+	 * @param totalA The total number of appointments if it's a course treatment, otherwise just set to 1.
+	 * @param currA The current appointment number out of the total appointments (set to 1 if not course treatment).
+	 * @throws MySQLIntegrityConstraintViolationException If appointment already exists.
+	 * @throws CommunicationsException when an error occurs whilst attempting connection.
+	 * @throws SQLException for any other error, could be incorrect parameters.
+	 */
 	public static void setAppointment(Timestamp start, Timestamp end, String userN, int patID, String nts, 
 			   AppointmentType treatmentN, int totalA, int currA) throws MySQLIntegrityConstraintViolationException, CommunicationsException, SQLException {
-		Appointment app = new Appointment(start, end, userN, patID, nts, treatmentN, totalA, currA);
+		new Appointment(start, end, userN, patID, nts, treatmentN, totalA, currA);
 	}
 	
-	public void deleteAppointment(Timestamp start, String userN) throws Exception {
+	/**
+	 * Deletes an appointment from the Appointments Table.
+	 * @param start Timestamp of when the appointment should start.
+	 * @param userN Username of staff member conducting the appointment.
+	 * @throws CommunicationsException when an error occurs whilst attempting connection.
+	 * @throws SQLException for any other error, could be incorrect parameters.
+	 */
+	public void deleteAppointment(Timestamp start, String userN) throws CommunicationsException, SQLException  {
 		Appointment app = new Appointment(start, userN);
-//		app.removeAppointment( start, userN);
+		app.removeAppointment();
 	}
-	
-	private static ResultSet execQuery(String query, Connection conn) {
-		ResultSet rs = null;
-		try {
-			conn = Database.getConnection();
-			Statement stmt = conn.createStatement();
-			rs = stmt.executeQuery(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return rs;
-	}
-	
-//	public static void main(String[] args) throws MySQLIntegrityConstraintViolationException, CommunicationsException, SQLException {
-//		setAppointment(java.sql.Timestamp.valueOf("2007-09-23 10:10:10.0"), java.sql.Timestamp.valueOf("2007-09-23 11:10:10.0"), "ayjee", 1, "abc", AppointmentType.CHECKUP, 1, 1);
-//		
-//	}
-	
 }
