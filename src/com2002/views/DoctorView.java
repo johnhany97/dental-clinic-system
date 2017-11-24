b9285dd26711cec830d2c0aed02b1f016fd37327
John Hany <johnhany97@gmail.com>
2017-11-24 05:19:02 +0000

    Improve DoctorsView Appointments list to follow new standard

--- src/com2002/views/DoctorView.java
+++ src/com2002/views/DoctorView.java
@@ -2,7 +2,7 @@ package com2002.views;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -11,8 +11,8 @@ import java.awt.event.KeyEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -20,7 +20,6 @@ import java.util.Date;
 import java.util.List;
 
 import javax.swing.BorderFactory;
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -31,7 +30,6 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 
 import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
@@ -55,7 +53,7 @@ public class DoctorView implements Screen {
 	private JLabel title;
 	private List<Appointment> appointments;
 	private List<JPanel> appointmentCards;
-	private JButton changeDayButton;
+	private JDatePickerImpl datePicker;
 	
 	public DoctorView(DisplayFrame frame, Doctor doctor) throws Exception {
 		this.frame = frame;
@@ -101,7 +99,7 @@ public class DoctorView implements Screen {
 		//date picker
 		UtilDateModel model = new UtilDateModel();
 		JDatePanelImpl datePanel = new JDatePanelImpl(model);
-		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel);
+		this.datePicker = new JDatePickerImpl(datePanel);
 		//set by default today
 		JLabel label = new JLabel("Viewing day: ");
 		label.setFont(new Font("Sans Serif", Font.PLAIN,
@@ -181,62 +179,88 @@ public class DoctorView implements Screen {
         frame.setJMenuBar(menuBar);
 	}
 	
-	@SuppressWarnings("deprecation")
 	private void addAppointment(Appointment appointment) {
-		//init
-		this.appointmentCards.add(new JPanel());
-		int index = this.appointmentCards.size() - 1;
-		Patient patient;
 		try {
+			this.appointmentCards.add(new JPanel());
+			int index = this.appointmentCards.size() - 1;
+			this.appointmentCards.get(index).setLayout(new BorderLayout());
+			this.appointmentCards.get(index).setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10), BorderFactory.createLineBorder(Color.black)));
+			//Get Appointment details
+			Patient patient;
 			patient = appointment.getPatient();
-			String patientName = patient.getTitle() + " " + patient.getFirstName() + " " + patient.getLastName();
 			String appointmentType = appointment.getAppointmentType();
-			Timestamp startTimeTs = appointment.getStartTime();
-			String startTime = String.valueOf(startTimeTs.getHours()) + ":" + String.valueOf(startTimeTs.getMinutes());
-			Timestamp endTimeTs = appointment.getEndTime();
-			String endTime = String.valueOf(endTimeTs.getHours()) + ":" + String.valueOf(endTimeTs.getMinutes());
-			String appointmentTime = startTime + " - " + endTime;
-			//layout
-			this.appointmentCards.get(index).setLayout(new BoxLayout(this.appointmentCards.get(index), BoxLayout.Y_AXIS));
-			this.appointmentCards.get(index).setMaximumSize(new Dimension(Integer.MAX_VALUE, frame.getFrameHeightStep() / 2));
-			// top content
-			JPanel topPanel = new JPanel();
-			topPanel.setLayout(new FlowLayout());
-			if (appointment.getAppointmentType().equals("Empty")) {
-				JLabel emptyLabel = new JLabel("Empty Appointment");
-				emptyLabel.setFont(new Font("Sans Serif", Font.PLAIN, 
-						DisplayFrame.FONT_SIZE / 2));
-				topPanel.add(emptyLabel);
-			} else {
-				JLabel patientNameLabel = new JLabel(patientName);
-				patientNameLabel.setFont(new Font("Sans Serif", Font.PLAIN,
-						DisplayFrame.FONT_SIZE / 2));
-				topPanel.add(patientNameLabel);
-				topPanel.add(Box.createHorizontalStrut(5));
-				topPanel.add(new JSeparator(SwingConstants.VERTICAL));
-				topPanel.add(Box.createHorizontalStrut(5));
-				JLabel appointmentTypeLabel = new JLabel(appointmentType);
-				appointmentTypeLabel.setFont(new Font("Sans Serif", Font.PLAIN,
-						DisplayFrame.FONT_SIZE / 2));
-				topPanel.add(appointmentTypeLabel);
+			String patientName = patient.getTitle() + " " + patient.getFirstName() + " " + patient.getLastName();
+			if (appointmentType.equals("Empty")) {
+				patientName = "Empty Appointment";
+				
 			}
-			topPanel.add(Box.createHorizontalStrut(5));
-			topPanel.add(new JSeparator(SwingConstants.VERTICAL));
-			topPanel.add(Box.createHorizontalStrut(5));
-			JLabel appointmentTimeLabel = new JLabel(appointmentTime);
-			appointmentTimeLabel.setFont(new Font("Sans Serif", Font.PLAIN,
+			String docName = this.doctor.getFirstName() + " " + this.doctor.getLastName();
+			String appointmentStatus = "Single Appointment";
+			if (appointment.getTotalAppointments() > 1) {
+				appointmentStatus = "Appointment " + appointment.getCurrentAppointment() + " of " + appointment.getTotalAppointments();
+			}
+			LocalDateTime startTime = appointment.getStartTime().toLocalDateTime();
+			String startString = String.format("%tH:%tM", startTime, startTime);
+			String startDayString = String.format("%tD", startTime);
+			LocalDateTime endTime = appointment.getEndTime().toLocalDateTime();
+			String endString = String.format("%tH:%tM", endTime, endTime);
+			String endDayString = String.format("%tD", endTime);
+			//Left section (contains time)
+			JPanel leftSection = new JPanel();
+			leftSection.setLayout(new BoxLayout(leftSection, BoxLayout.Y_AXIS));
+			JLabel start = new JLabel("Start", SwingConstants.CENTER);
+			start.setFont(new Font("Sans Serif", Font.BOLD,
+					DisplayFrame.FONT_SIZE / 3));
+			leftSection.add(start);
+			JLabel startT = new JLabel(startString, SwingConstants.CENTER);
+			startT.setFont(new Font("Sans Serif", Font.PLAIN,
 					DisplayFrame.FONT_SIZE / 2));
-			topPanel.add(appointmentTimeLabel);
-			this.appointmentCards.get(index).add(topPanel);
-			// bottom content			
-			JPanel bottomPanel = new JPanel();
-			bottomPanel.setLayout(new FlowLayout());
-			JButton startAppointment = new JButton("Start Appointment");
-			startAppointment.setFont(new Font("Sans Serif", Font.PLAIN,
+			leftSection.add(startT);
+			JLabel startD = new JLabel(startDayString, SwingConstants.CENTER);
+			startD.setFont(new Font("Sans Serif", Font.PLAIN,
+					DisplayFrame.FONT_SIZE / 3));
+			leftSection.add(startD);
+			JLabel end = new JLabel("End", SwingConstants.CENTER);
+			end.setFont(new Font("Sans Serif", Font.BOLD,
+					DisplayFrame.FONT_SIZE / 3));
+			leftSection.add(end);
+			JLabel endT = new JLabel(endString, SwingConstants.CENTER);
+			endT.setFont(new Font("Sans Serif", Font.PLAIN,
 					DisplayFrame.FONT_SIZE / 2));
-			bottomPanel.add(startAppointment);
-			//event listener for the button
-			startAppointment.addActionListener(new ActionListener() {
+			leftSection.add(endT);
+			JLabel endD = new JLabel(endDayString, SwingConstants.CENTER);
+			endD.setFont(new Font("Sans Serif", Font.PLAIN,
+					DisplayFrame.FONT_SIZE / 3));
+			leftSection.add(endD);
+			leftSection.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+			this.appointmentCards.get(index).add(leftSection, BorderLayout.WEST);
+			//right section (rest of appointment details)
+			JPanel topRightSection = new JPanel();
+			topRightSection.setLayout(new BoxLayout(topRightSection, BoxLayout.Y_AXIS));
+			topRightSection.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+			JLabel patientT = new JLabel(patientName, SwingConstants.LEFT);
+			patientT.setFont(new Font("Sans Serif", Font.PLAIN,
+					DisplayFrame.FONT_SIZE / 2));
+			topRightSection.add(patientT);
+			topRightSection.setAlignmentX(Component.LEFT_ALIGNMENT);
+			JLabel typeAndStatus = new JLabel(appointmentType + " | " + appointmentStatus);
+			typeAndStatus.setFont(new Font("Sans Serif", Font.PLAIN,
+					DisplayFrame.FONT_SIZE / 3));
+			topRightSection.add(typeAndStatus);
+			JLabel doctorT = new JLabel("Doctor: " + docName);
+			doctorT.setFont(new Font("Sans Serif", Font.PLAIN,
+					DisplayFrame.FONT_SIZE / 3));
+			topRightSection.add(doctorT);
+			JPanel bottomRightSection = new JPanel();
+			bottomRightSection.setLayout(new FlowLayout());
+			bottomRightSection.setAlignmentX(Component.LEFT_ALIGNMENT);
+			//Buttons
+			//Appointment Details Button
+			JButton detailsButton = new JButton("View");
+			detailsButton.setFont(new Font("Sans Serif", Font.PLAIN,
+					DisplayFrame.FONT_SIZE / 3));
+			bottomRightSection.add(detailsButton);
+			detailsButton.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					DisplayFrame newFrame = new DisplayFrame();
@@ -245,22 +269,92 @@ public class DoctorView implements Screen {
 					newFrame.setDisplayedPanel(appointmentView.getPanel());
 				}
 			});
+			//Patient Info Button
+			JButton patientInfoButton = new JButton("Patient Info");
+			patientInfoButton.setFont(new Font("Sans Serif", Font.PLAIN,
+					DisplayFrame.FONT_SIZE / 3));
+			patientInfoButton.putClientProperty("id", patient.getPatientID());
+			patientInfoButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					try {
+						Patient patient = new Patient((int) patientInfoButton.getClientProperty("id"));
+						DisplayFrame patientViewFrame = new DisplayFrame();
+						PatientView patientView = new PatientView(patientViewFrame, patient);
+						patientViewFrame.setDisplayedPanel(patientView.getPanel());
+					} catch (CommunicationsException e) {
+	
+						e.printStackTrace();
+						JOptionPane.showMessageDialog(frame,
+							    "Database error. Check your internet connnection.",
+							    "Error fetching patient",
+							    JOptionPane.ERROR_MESSAGE);
+					} catch (SQLException e) {
+						JOptionPane.showMessageDialog(frame,
+							    e.getMessage(),
+							    "Error fetching patient",
+							    JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			});
+			JButton deleteButton = new JButton("Cancel");
+			deleteButton.setFont(new Font("Sans Serif", Font.PLAIN, 
+					DisplayFrame.FONT_SIZE / 3));
+			deleteButton.putClientProperty("Appointment", appointment);
+			deleteButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					Appointment appointment = (Appointment) deleteButton.getClientProperty("Appointment");
+					int selectedOption = JOptionPane.showConfirmDialog(null, "Do you wanna cancel this appointment?", "Choose", JOptionPane.YES_NO_OPTION); 
+		    		if (selectedOption == JOptionPane.YES_OPTION) {
+		    			try {
+							appointment.removeAppointment();
+							//refresh
+							//get day
+							Date selectedDate = (Date) datePicker.getModel().getValue();
+							//change list of appointments
+							appointments = Schedule.getAppointmentsByDoctorAndDay(doctor, selectedDate);
+							//change title
+							LocalDate localDate = selectedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+							int year  = localDate.getYear();
+							int month = localDate.getMonthValue();
+							int day   = localDate.getDayOfMonth();
+							String dayString = String.valueOf(day) + "/" + String.valueOf(month) + "/" + String.valueOf(year);
+							title.setText("Appointments - " + dayString);
+							appointmentsPanel.removeAll();
+							appointmentsPanel.repaint();
+							appointmentCards.clear();
+							for  (int i = 0; i < appointments.size(); i++) {
+								addAppointment(appointments.get(i));
+							}
+							frame.revalidate();
+						} catch (SQLException e) {
+							JOptionPane.showMessageDialog(frame,
+								    e.getMessage(),
+								    "Error deleting appointment",
+								    JOptionPane.ERROR_MESSAGE);
+						}
+		    		}
+				}
+			});
+			bottomRightSection.add(patientInfoButton);
+			bottomRightSection.add(deleteButton);
 			if (appointment.getAppointmentType().equals("Empty")) {
-				startAppointment.setEnabled(false);
+				detailsButton.setEnabled(false);
+				patientInfoButton.setEnabled(false);
 			}
-			this.appointmentCards.get(index).add(bottomPanel);
-			//add panel to main panel
+			bottomRightSection.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+			//TODO: Action listener
+			JPanel rightSection = new JPanel();
+			rightSection.setLayout(new BoxLayout(rightSection, BoxLayout.Y_AXIS));
+			rightSection.add(topRightSection);
+			rightSection.add(bottomRightSection);
+			this.appointmentCards.get(index).add(rightSection, BorderLayout.CENTER);
 			this.appointmentsPanel.add(this.appointmentCards.get(index));
-			JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
-			separator.setMaximumSize(new Dimension(Integer.MAX_VALUE, 1));
-			separator.setBackground(Color.black);
-			this.appointmentsPanel.add(separator);
-			
-		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(frame,
-				    "Error connecting to the database. Check internet connection.",
-				    "Error",
-				    JOptionPane.ERROR_MESSAGE);
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
 	
