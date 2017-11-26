9b00f82569d6016881f289de583caae9958670ce
John Hany <johnhany97@gmail.com>
2017-11-26 03:39:55 +0000

    SQL Table creation statements look nicer like this

--- src/com2002/utils/DatabaseTables.java
+++ src/com2002/utils/DatabaseTables.java
@@ -141,50 +141,85 @@ public class DatabaseTables {
 		// connect
 		Connection conn = Database.getConnection();
 		// tables schema
-		String treatmentsTable = "CREATE TABLE `Treatments` (\r\n" + "	`Name` VARCHAR(30) NOT NULL,\r\n"
-				+ "	`Price` DOUBLE,\r\n" + "	PRIMARY KEY(`Name`)\r\n" + ")";
-		String appointmentsTable = "CREATE TABLE `Appointments` (\r\n" + "	`StartDate`	DATETIME NOT NULL,\r\n" + // YYYY-MM-DD
-																													// HH:MM:SS
-				"	`EndDate`	DATETIME NOT NULL,\r\n" + "	`Username`	VARCHAR(15) NOT NULL,\r\n"
-				+ "	`Type`	VARCHAR(30) NOT NULL,\r\n" + "	`PatientID`	INTEGER,\r\n" + "	`Notes`	VARCHAR(1000),\r\n"
-				+ "	`TotalAppointments`	INTEGER,\r\n" + "	`CurrentAppointment`	INTEGER,\r\n"
-				+ "   `Paid`  BIT DEFAULT 0,\r\n" + "	PRIMARY KEY(`StartDate`,`Username`),\r\n"
+		String treatmentsTable = "CREATE TABLE `Treatments` (\r\n" 
+				+ "	`Name` 	VARCHAR(30) NOT NULL,\r\n"
+				+ "	`Price` DOUBLE,\r\n" 
+				+ "	PRIMARY KEY(`Name`)\r\n" 
+				+ ")";
+		String appointmentsTable = "CREATE TABLE `Appointments` (\r\n"
+				+ "	`StartDate`				DATETIME NOT NULL,\r\n"
+				+ "	`EndDate`				DATETIME NOT NULL,\r\n"
+				+ "	`Username`				VARCHAR(15) NOT NULL,\r\n"
+				+ "	`Type`					VARCHAR(30) NOT NULL,\r\n"
+				+ "	`PatientID`				INTEGER,\r\n"
+				+ "	`Notes`					VARCHAR(1000),\r\n"
+				+ "	`TotalAppointments`		INTEGER,\r\n"
+				+ "	`CurrentAppointment`	INTEGER,\r\n"
+				+ " `Paid` 					BIT DEFAULT 0,\r\n"
+				+ "	PRIMARY KEY(`StartDate`,`Username`),\r\n"
 				+ "	FOREIGN KEY(`Username`) REFERENCES Employees(Username),\r\n"
 				+ "	FOREIGN KEY(`PatientID`) REFERENCES Patients(PatientID),\r\n"
-				+ "   FOREIGN KEY(`Type`) REFERENCES AppointmentTypes(Name)" + ")";
+				+ " FOREIGN KEY(`Type`) REFERENCES AppointmentTypes(Name)" 
+				+ ")";
 		String appointmentTreatmentTable = "CREATE TABLE `AppointmentTreatment` (\r\n"
-				+ "   `StartDate`     DATETIME NOT NULL,\r\n" + "   `Username`      VARCHAR(15) NOT NULL,\r\n"
-				+ "   `TreatmentName` VARCHAR(30) NOT NULL,\r\n"
-				+ "   FOREIGN KEY(`StartDate`, `Username`) References Appointments(`StartDate`, `Username`),\r\n"
-				+ "   FOREIGN KEY(`TreatmentName`) References Treatments(`Name`)" + ")";
+				+ " `StartDate`     DATETIME NOT NULL,\r\n" 
+				+ " `Username`      VARCHAR(15) NOT NULL,\r\n"
+				+ " `TreatmentName` VARCHAR(30) NOT NULL,\r\n"
+				+ " FOREIGN KEY(`StartDate`, `Username`) References Appointments(`StartDate`, `Username`),\r\n"
+				+ " FOREIGN KEY(`TreatmentName`) References Treatments(`Name`)" 
+				+ ")";
 		String appointmentTypesTable = "CREATE TABLE `AppointmentTypes` (\r\n"
-				+ "   `Name`      VARCHAR(30) NOT NULL,\r\n" + "   `Price`     DOUBLE,\r\n"
-				+ "   PRIMARY KEY(`Name`)\r\n" + ")";
-		String employeesTable = "CREATE TABLE `Employees` (\r\n" + "	`FirstName`	VARCHAR(30) NOT NULL,\r\n"
-				+ "	`LastName`	VARCHAR(30) NOT NULL,\r\n" + "	`Username`	VARCHAR(15) NOT NULL,\r\n"
-				+ "	`Password`	VARCHAR(30) NOT NULL,\r\n" + "	`Role`	VARCHAR(30) NOT NULL,\r\n"
-				+ "	PRIMARY KEY(`Username`)\r\n" + ")";
-		String healthPlansTable = "CREATE TABLE `HealthPlans` (\r\n" + "	`Name`	VARCHAR(30) NOT NULL,\r\n"
-				+ "	`Price`	REAL,\r\n" + "	`CheckUpLevel`	INTEGER NOT NULL,\r\n"
-				+ "	`HygieneLevel`	INTEGER NOT NULL,\r\n" + "	`RepairLevel`	INTEGER NOT NULL,\r\n"
-				+ "	PRIMARY KEY(`Name`)\r\n" + ")";
+				+ " `Name`      VARCHAR(30) NOT NULL,\r\n"
+				+ " `Price`     DOUBLE,\r\n"
+				+ " PRIMARY KEY(`Name`)\r\n"
+				+ ")";
+		String employeesTable = "CREATE TABLE `Employees` (\r\n"
+				+ "	`FirstName`	VARCHAR(30) NOT NULL,\r\n"
+				+ "	`LastName`	VARCHAR(30) NOT NULL,\r\n"
+				+ "	`Username`	VARCHAR(15) NOT NULL,\r\n"
+				+ "	`Password`	VARCHAR(30) NOT NULL,\r\n"
+				+ "	`Role`		VARCHAR(30) NOT NULL,\r\n"
+				+ "	PRIMARY KEY(`Username`)\r\n" 
+				+ ")";
+		String healthPlansTable = "CREATE TABLE `HealthPlans` (\r\n"
+				+ "	`Name`			VARCHAR(30) NOT NULL,\r\n"
+				+ "	`Price`			REAL,\r\n"
+				+ "	`CheckUpLevel`	INTEGER NOT NULL,\r\n"
+				+ "	`HygieneLevel`	INTEGER NOT NULL,\r\n"
+				+ "	`RepairLevel`	INTEGER NOT NULL,\r\n"
+				+ "	PRIMARY KEY(`Name`)\r\n"
+				+ ")";
 		String patientHealthPlanTable = "CREATE TABLE `PatientHealthPlan` (\r\n"
-				+ "	`PatientID`	INTEGER NOT NULL,\r\n" + "	`HealthPlanName`	VARCHAR(30),\r\n"
-				+ "	`CheckUpUsed`	INTEGER DEFAULT 0,\r\n" + "	`HygieneUsed`	INTEGER DEFAULT 0,\r\n"
-				+ "	`RepairUsed`	INTEGER DEFAULT 0,\r\n" + "	`DateJoined`	DATE NOT NULL,\r\n"
+				+ "	`PatientID`	I		NTEGER NOT NULL,\r\n"
+				+ "	`HealthPlanName`	VARCHAR(30),\r\n"
+				+ "	`CheckUpUsed`		INTEGER DEFAULT 0,\r\n"
+				+ "	`HygieneUsed`		INTEGER DEFAULT 0,\r\n"
+				+ "	`RepairUsed`		INTEGER DEFAULT 0,\r\n"
+				+ "	`DateJoined`		DATE NOT NULL,\r\n"
 				+ "	PRIMARY KEY(`PatientID`,`HealthPlanName`),\r\n"
 				+ "	FOREIGN KEY(`PatientID`) REFERENCES Patients(PatientID),\r\n"
-				+ "	FOREIGN KEY(`HealthPlanName`) REFERENCES HealthPlans(Name)\r\n" + ")";
-		String patientsTable = "CREATE TABLE `Patients` (\r\n" + "	`PatientID`	INTEGER,\r\n"
-				+ "   `Title`     VARCHAR(4),\r\n" + "	`FirstName`	VARCHAR(30) NOT NULL,\r\n"
-				+ "	`LastName`	VARCHAR(30) NOT NULL,\r\n" + "	`DateOfBirth`	DATE NOT NULL,\r\n"
-				+ "	`PhoneNumber`	VARCHAR(20),\r\n" + "	`HouseNumber`	VARCHAR(30) NOT NULL,\r\n"
-				+ "	`Postcode`	VARCHAR(8) NOT NULL,\r\n" + "   PRIMARY KEY (`PatientID`),\r\n"
-				+ "	FOREIGN KEY(`HouseNumber`, `Postcode`) REFERENCES Address(HouseNumber, Postcode)\r\n" + ");";
-		String addressTable = "CREATE TABLE `Address` (\r\n" + "	`HouseNumber`	VARCHAR(30) NOT NULL,\r\n"
-				+ "	`StreetName`	VARCHAR(30) NOT NULL,\r\n" + "	`District`	VARCHAR(30) NOT NULL,\r\n"
-				+ "	`City`	VARCHAR(30) NOT NULL,\r\n" + "	`Postcode`	VARCHAR(8) NOT NULL,\r\n"
-				+ "	PRIMARY KEY(`HouseNumber`,`Postcode`)\r\n" + ")";
+				+ "	FOREIGN KEY(`HealthPlanName`) REFERENCES HealthPlans(Name)\r\n"
+				+ ")";
+		String patientsTable = "CREATE TABLE `Patients` (\r\n"
+				+ "	`PatientID`		INTEGER,\r\n"
+				+ " `Title`     	VARCHAR(4),\r\n"
+				+ "	`FirstName`		VARCHAR(30) NOT NULL,\r\n"
+				+ "	`LastName`		VARCHAR(30) NOT NULL,\r\n"
+				+ "	`DateOfBirth`	DATE NOT NULL,\r\n"
+				+ "	`PhoneNumber`	VARCHAR(20),\r\n"
+				+ "	`HouseNumber`	VARCHAR(30) NOT NULL,\r\n"
+				+ "	`Postcode`		VARCHAR(8) NOT NULL,\r\n"
+				+ " PRIMARY KEY (`PatientID`),\r\n"
+				+ "	FOREIGN KEY(`HouseNumber`, `Postcode`) REFERENCES Address(HouseNumber, Postcode)\r\n"
+				+ ");";
+		String addressTable = "CREATE TABLE `Address` (\r\n"
+				+ "	`HouseNumber`	VARCHAR(30) NOT NULL,\r\n"
+				+ "	`StreetName`	VARCHAR(30) NOT NULL,\r\n"
+				+ "	`District`		VARCHAR(30) NOT NULL,\r\n"
+				+ "	`City`			VARCHAR(30) NOT NULL,\r\n"
+				+ "	`Postcode`		VARCHAR(8) NOT NULL,\r\n"
+				+ "	PRIMARY KEY(`HouseNumber`,`Postcode`)\r\n"
+				+ ")";
 		try { // Run the above statements
 			Statement stmt = conn.createStatement();
 			stmt.executeUpdate(treatmentsTable);
